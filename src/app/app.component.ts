14f1b089592aba1c7a1e91bff9c6de198c55d534
Lawrence Dovin <lawrencedovin@gmail.com>
2022-09-15 12:00:46 -0400

    added files from original portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lawrence-dovin';
+  title = 'lawrence-dovin-portfolio';
+
+  // constructor(private viewportScroller: ViewportScroller) {}
+
+  // public onClick(elementID: string): void {
+  //   this.viewportScroller.scrollToAnchor(elementID);
+  // }
+
+  // public scroll(elementID: string) {
+  //   let el = document.querySelectorAll(elementID)[0];
+  //   el.scrollIntoView({behavior: 'smooth'});
+  //   console.log('hwadsd')
+  // }
+
+  // scroll() {
+  //   let el = document.querySelectorAll("#contact-form")[0];
+  //   el.scrollIntoView({behavior: 'smooth'});
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
+
+import { AppComponent } from './app.component';
+
+// ANGULAR MODULES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+
+// CUSTOM MODULES
+import { SharedModule } from './shared/shared.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactService } from './services/contact.service';
+import { HomeModule } from './modules/home/home.module';
+import { PortfolioModule } from './modules/portfolio/portfolio.module';
+import { ResumeModule } from './modules/resume/resume.module';
+import { AboutModule } from './modules/about/about.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
+    // ANGULAR MODULES
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // CUSTOM MODULES
+    SharedModule,
+    HomeModule,
+    PortfolioModule,
+    ResumeModule,
+    AboutModule
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
